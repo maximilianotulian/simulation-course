87f063203a581a54a13810d45166f65b08505604
Maximiliano Tulian <maxtulian93@hotmail.com>
2015-11-03 21:33:45 -0300

    [Maximiliano Tulian] - update inicializacion, principal

--- src/espera/Inicializacion.java
+++ src/espera/Inicializacion.java
@@ -10,14 +10,17 @@ public class Inicializacion {
                 //La otra esta formado por hora que seria la hora del tipo de evento
                 modelo.setMediaArribo(0.5);
                 modelo.setMediaPartida(0.25);
-                Lev a = new Lev(Arribo.C(), Libreria.distExp(modelo.getMediaArribo()),true);
+                
+                //primer arribo
+                Lev a = new Lev(Arribo.C(), Libreria.distExp(modelo.getMediaArribo()), true);
                 modelo.getLEV().add(a);
-                a = new Lev(Partida.C(), -1,false);
-                modelo.getLEV().add(a);
+                
                 //creamos la primer partida y la guardamos en LEV
                 //Seteamos el tiemp -1 para indicar que no puede ocurrir
+                a = new Lev(Partida.C(), -1,false);
+                modelo.getLEV().add(a);
+                
+                //setiamos el tiempo final de simulación
                 modelo.setTFS(TFS);
 	}
-	
-	
 }
