d534eeb4f1a2a17ad84dd7d091a90ace942f06ea
Maximiliano Tulian <maxtulian93@hotmail.com>
2015-11-03 22:02:50 -0300

    [Maximiliano Tulian] - fix upper limit bug in tiempos.java

--- src/espera/Tiempos.java
+++ src/espera/Tiempos.java
@@ -12,12 +12,12 @@ public static char busca(Espera modelo){
     char evento = 'z';
     ArrayList<Espera.Lev> jeje = modelo.getLEV();
     int indice = 0;
-    double hora = 100000000; 
+    double hora = modelo.getTFS() + 1;
+    
     for (int i = 0; i < jeje.size(); i++) {
         if (jeje.get(i).usado && jeje.get(i).hora < hora && modelo.getReloj() < jeje.get(i).hora) { 
             indice = i;
             hora = jeje.get(i).hora;
-            System.out.println(hora);
             }
     }
     jeje.get(indice).usado = false;
@@ -25,5 +25,4 @@ public static char busca(Espera modelo){
     evento = jeje.get(indice).C;
     return evento;
 }
-    
 }
